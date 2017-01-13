36333040b9dae55f5a28bb3fe6ce0040aca59a9b
Piotr Balut <p.balut@samsung.com>
2017-01-13 09:11:26 +0100

    Updated ffmpeg to library to webports commit 4b49148 (ffmpeg 3.2.2).
    Change-Id: I4b7353e35ef6b6e966047a5df4d5dcb87a093525

--- third_party/include/libavutil/avassert.h
+++ third_party/include/libavutil/avassert.h
@@ -38,14 +38,14 @@
     if (!(cond)) {                                                      \
         av_log(NULL, AV_LOG_PANIC, "Assertion %s failed at %s:%d\n",    \
                AV_STRINGIFY(cond), __FILE__, __LINE__);                 \
-        fprintf(stderr,"file:%s,line:%d\n",__FILE__,__LINE__); abort();                                                        \
+        abort();                                                        \
     }                                                                   \
 } while (0)
 
 
 /**
  * assert() equivalent, that does not lie in speed critical code.
- * These asserts() thus can be enabled without fearing speedloss.
+ * These asserts() thus can be enabled without fearing speed loss.
  */
 #if defined(ASSERT_LEVEL) && ASSERT_LEVEL > 0
 #define av_assert1(cond) av_assert0(cond)
@@ -59,8 +59,17 @@
  */
 #if defined(ASSERT_LEVEL) && ASSERT_LEVEL > 1
 #define av_assert2(cond) av_assert0(cond)
+#define av_assert2_fpu() av_assert0_fpu()
 #else
 #define av_assert2(cond) ((void)0)
+#define av_assert2_fpu() ((void)0)
 #endif
+
+/**
+ * Assert that floating point opperations can be executed.
+ *
+ * This will av_assert0() that the cpu is not in MMX state on X86
+ */
+void av_assert0_fpu(void);
 
 #endif /* AVUTIL_AVASSERT_H */
